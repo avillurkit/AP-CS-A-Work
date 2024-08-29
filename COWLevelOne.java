277fe905ab895e47eeca8d8bb1f13dd34de982db
GA2 <87157176+avillurkit@users.noreply.github.com>
2024-08-29 08:24:34 -0400

    Create COW1 LevelOne.java

---
+++ COWLevelOne.java
@@ -0,0 +1,22 @@
+public class Calculator1
+{
+    public static double calcAreaOfSquare(double length){
+        return length * length;
+    }
+    
+    public static double calcResistanceInSeries(double r1, double r2){
+        return r1 + r2;
+    }
+    
+    public static double calcAreaOfRectangle(double length, double width){
+        return length * width;
+    }
+    
+    public static double calcMilesPerHour(double miles, double hour){
+        return miles/hour;
+    }
+    
+    public static double getProfit(double income, double expenses){
+        return income - expenses;
+    }
+}
